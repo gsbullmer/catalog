225cf599d3d779a9be8fbbe0755b7b9a311206d4
Greg Bullmer <gsbullmer@gmail.com>
2015-12-03 16:57:56 -0600

    Debug: Update facebook login service

--- application.py
+++ application.py
@@ -381,16 +381,17 @@ def fbconnect():
         return response
     access_token = request.data
 
-    app_id = json.loads(open(root + '/static/secrets/fb_client_secrets.json',
-                             'r').read())['web']['app_id']
-    app_secret = json.loads(open(root + '/static/secrets/fb_client_secrets.json',
-                                 'r').read())['web']['app_secret']
+    app_id = json.loads(
+        open(root + '/static/secrets/fb_client_secrets.json',
+             'r').read())['web']['app_id']
+    app_secret = json.loads(
+        open(root + '/static/secrets/fb_client_secrets.json',
+             'r').read())['web']['app_secret']
     url = 'https://graph.facebook.com/oauth/access_token?\
         grant_type=fb_exchange_token&client_id=%s&client_secret=%s&\
         fb_exchange_token=%s' % (app_id, app_secret, access_token)
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
-    print result
 
     userinfo_url = 'https://graph.facebook.com/v2.4/me'
     token = result.split('&')[0]
